9b22d5a306a7e630fda20cf1289280901ce09420
Vrund Patel <vrundpatel876@gmail.com>
2024-11-13 16:27:50 +0530

    Update q2.php

--- q2.php
+++ q2.php
@@ -1,15 +1,27 @@
-<?php
-if(isset($_POST['submit']))
-{
-	$name=$_POST['name'];
-	setcookie("name",$name,time()+3600);
-}
-?>
+<<!DOCTYPE html>
 <html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
 <body>
-	<form method="POST" action="qe2.php">
-		Name:<input type="text" name="name"/>
-		<input type="submit" name="submit">
+	<form method="POST">
+		Name:
+		<input type="name" name="cookie_value" /><br>
+		<input type="submit" name="submit"/>
 	</form>
+	<?php
+	if(isset($_POST['submit'])){
+	$cookie_name = "username";
+	$cookie_value = $_POST['cookie_value']; 
+	setcookie($cookie_name, $cookie_value, time() + 3600, "/"); 
+
+	header("Location: qe2.php"); 
+	exit();
+	}
+	?>
 </body>
 </html>
+
+
