be9b3a073a41647833be66e53be6a5760dd6f556
Vrund Patel <vrundpatel876@gmail.com>
2024-11-13 16:27:26 +0530

    Update q1.php

--- q1.php
+++ q1.php
@@ -1,16 +1,26 @@
-<?php
-if(isset($_POST['submit']))
-{
-	$name=$_POST['name'];
-	setcookie("name",$name,time() -3600, "/");
-	echo "cookie 'name is set to:".$name;
-}
-?>
+<!DOCTYPE html>
 <html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
 <body>
-	<form method="POST" action="qe1.php">
-		Name:<input type="text" name="name" required/>
-		<input type="submit" name="submit"/>
-	</form>
+    <form method="POST">
+        Name:
+        <input type="text" name="name"/>
+        <input type="submit" name="submit" value="submit"/>
+        <?php
+        if(isset($_POST['submit']))
+        {
+            $cookie_name="user";
+            $cookie_value=$_POST['name'];
+
+            setcookie($cookie_name,$cookie_value,time()+3600,"/");
+            header("Location: qe1.php");
+            exit();
+        }
+        ?>
+    </form>
 </body>
 </html>
